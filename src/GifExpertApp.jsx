e4e944fd7a42aea010e8b4715a472971e773500f
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-07 03:39:41 -0400

    Comunicación entre componentes
    Pasar setState como argumento de GifExpertApp(padre) a AddCategory(hijo)

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+// import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
 
@@ -11,32 +12,35 @@ export const GifExpertApp = () => {
    */
   const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
 
-  const onAddCategory = () => {
-    /**
-     * Hay muchas formas de agregar un nuevo elemento a un arreglo usando el hook useState.
-     * 1. crear una variable que contenga el nuevo arreglo y luego pasarlo al hook.
-     * const newCategories = [...categories, 'Valorant'];
-     * setCategories(newCategories)
-     * 
-     * 2. Usar el callback del hook useState.
-     * setCategories( cat => [ ...cat, 'Valorant' ] );
-     * 
-     * 3. Pasar el nuevo arreglo directamente al hook.
-     * setCategories([ ...categories, 'Valorant' ]);
-     */
-    setCategories([ 'Valorant', ...categories ]);
-  }
+  /**
+   * Hay muchas formas de agregar un nuevo elemento a un arreglo usando el hook useState.
+   * 1. crear una variable que contenga el nuevo arreglo y luego pasarlo al hook.
+   * const newCategories = [...categories, 'Valorant'];
+   * setCategories(newCategories)
+   * 
+   * 2. Usar el callback del hook useState.
+   * setCategories( cat => [ ...cat, 'Valorant' ] );
+   * 
+   * 3. Pasar el nuevo arreglo directamente al hook.
+   * setCategories([ ...categories, 'Valorant' ]);
+   */
+  // const onAddCategory = () => {
+  //   setCategories([ 'Valorant', ...categories ]);
+  // }
 
+  /**
+   * en <AddCategory setCategories={ setCategories } /> tenemos que pasar la función setCategories como propiedad.
+   * esta función se encarga de agregar un nuevo elemento al arreglo de categorías.
+   */
   return (
     <>
       {/* título */}
       <h1>GifExpertApp</h1>
 
       {/* Input */}
-      <AddCategory />
+      <AddCategory setCategories={ setCategories } />
 
       {/* Listado de Gif */}
-      <button onClick={ onAddCategory }>Agregar</button>
       <ol>
         { 
           categories.map( category => {
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,6 +1,21 @@
 import { useState } from "react";
+import PropTypes from 'prop-types'
 
-export const AddCategory = () => {
+/**
+ * setCategories es una función que se pasa como argumento al componente.
+ * esta función se encarga de agregar un nuevo elemento al arreglo de categorías.
+ * 
+ * el componente AddCategory recibe un objeto con la propiedad setCategories.
+ * esta propiedad es una función que se encarga de agregar un nuevo elemento al arreglo de categorías.
+ * 
+ * setCategories nos marca un error porque no está definido en el componente AddCategory.
+ * para solucionar este error, debemos agregar setCategories como argumento de la función AddCategory.
+ * con propTypes podemos definir las propiedades que recibe un componente.
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
+export const AddCategory = ({ setCategories }) => {
 
   const [ inputValue, setInputValue ] = useState('');
 
@@ -18,17 +33,33 @@ export const AddCategory = () => {
   }
 
   /**
-   * para evitar que el formulario se envíe automáticamente, podemos usar el evento preventDefault.
+   * event.preventDefault() evita que el formulario se envíe automáticamente.
+   * 
+   * también podemos validar que el valor del input no esté vacío.
+   * si el valor del input está vacío, retornamos un return para que la función no haga nada.
+   * 
+   * para agregar un nuevo elemento al arreglo, podemos usar el operador spread.
+   * primero agregamos el nuevo valor y luego el resto de los elementos del arreglo.
+   * prevState es el estado anterior del arreglo. (no necesariamente se debe llamar prevState)
+   * setCategories(prevState => [ inputValue, ...prevState ]);
+   * 
+   * finalmente, limpiamos el input.
+   * setInputValue('');
    * 
    * @param {*} event 
    */
   const onSubmit = ( event ) => {
     event.preventDefault();
-    console.log(inputValue);
+    if ( inputValue.trim().length <= 1 ) return;
+
+    setCategories(prevState => [ inputValue, ...prevState ]);
+    setInputValue('');
   }
 
   /**
    * onSubmit es una función que se ejecuta cuando se envía el formulario.
+   * 
+   * onInputChange es una función que se ejecuta cuando se cambia el valor del input.
    * 
    * cuando en una función de flecha el primer argumento es el mismo
    * que se pasa a otra función, podemos simplificarlo de la siguiente forma:
@@ -36,7 +67,7 @@ export const AddCategory = () => {
    * A:  { onInputChange }
    */
   return (
-    <form onSubmit={ (event) => onSubmit(event) }>
+    <form onSubmit={ onSubmit }>
       <input
         type="text"
         placeholder="Buscar gifs"
@@ -45,4 +76,8 @@ export const AddCategory = () => {
       />
     </form>
   )
+}
+
+AddCategory.propTypes = {
+  setCategories: PropTypes.func.isRequired
 }
