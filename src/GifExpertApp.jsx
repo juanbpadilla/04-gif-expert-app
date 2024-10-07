105b7347e99bf19b78796815f39d57cc958d4559
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-06 23:01:51 -0400

    Creando una lista de categorías

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,5 +1,17 @@
+import { useState } from "react"
 
 export const GifExpertApp = () => {
+
+  /**
+   * ⚠️ IMPORTANTE:
+   * Los hooks son funciones que nos permiten conectar el estado y el ciclo de vida de los componentes funcionales.
+   * estos son pocisionales, es decir, el orden en el que se declaran es importante.
+   * no se pueden llamar condicionalmente, ni dentro de un loop. Solo se pueden llamar en el nivel más alto de un componente.
+   */
+  const [ categories, setCategories ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+  console.log(categories);
+
   return (
     <>
       {/* título */}
@@ -8,6 +20,13 @@ export const GifExpertApp = () => {
       {/* Input */}
 
       {/* Listado de Gif */}
+      <ol>
+        { 
+          categories.map( category => {
+            return <li key={ category }>{ category }</li>
+          }) 
+        }
+      </ol>
         {/* Gif Item */}
 
     </>
