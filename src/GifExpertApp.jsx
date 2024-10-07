54e4204ad6556f6507eb72e5b2386335e08a1c39
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-06 22:36:48 -0400

    GitExpertApp - Component

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -2,7 +2,14 @@
 export const GifExpertApp = () => {
   return (
     <>
+      {/* título */}
       <h1>GifExpertApp</h1>
+
+      {/* Input */}
+
+      {/* Listado de Gif */}
+        {/* Gif Item */}
+
     </>
   )
 }
