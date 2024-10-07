896faa8d58d25d15ae11e6c62e4c13fbe7857b10
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-07 08:54:32 -0400

    useEffect
    Refactorizar función de obtención de gifs y ajustar límite de resultados

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types'
-import { gefGifts } from '../helpers/getGifs';
+import { getGifts } from '../helpers/getGifs';
+import { useState, useEffect } from 'react';
 
 
 /**
@@ -8,16 +9,33 @@ import { gefGifts } from '../helpers/getGifs';
  * lo cual es una mala práctica, porque estamos haciendo una petición a la API de Giphy cada vez que se renderiza el componente.
  * lo correcto sería llamar a la función gefGifts(category) dentro de un useEffect.
  * 
+ * useEffect es un hook que nos permite ejecutar efectos secundarios en componentes funcionales.
+ * ..es decir, nos permite ejecutar código que no tiene que ver con la representación del componente.
+ * 
+ * useEffect recibe dos argumentos: una función y un arreglo de dependencias.
+ * ..la función que recibe useEffect se ejecuta cada vez que el componente se renderiza.
+ * ..y el arreglo de dependencias es un arreglo que contiene las variables que useEffect debe observar.
+ * si el arreglo de dependencias está vacío, la función que recibe useEffect se ejecuta solo una vez, cuando el componente se monta.
+ * 
+ * 
+ * 
  * @param {*} param0 
  * @returns 
  */
 export const GifGrid = ({ category }) => {
+
+  const [counter, setCounter] = useState(10)
+
+  useEffect( () => {
+    getGifts(category);
+  }, [])
   
-  gefGifts(category);
 
   return (
     <>
       <h3>{ category }</h3>
+      <h5>{ counter }</h5>
+      <button onClick={() => setCounter(counter+1)}>+1</button>
     </>
   )
 }
--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -18,10 +18,10 @@
  * @param {*} category 
  * @returns 
  */
-export const gefGifts = async( category ) => {
+export const getGifts = async( category ) => {
 
   const apiKey = import.meta.env.VITE_APP_GIPHY_API_KEY;
-  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=20`;
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${category}&limit=10`;
   const resp = await fetch( url );
   const { data } = await resp.json();
 
