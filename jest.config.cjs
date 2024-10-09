a109b91da0c708a1adf26c5caa7869a589e07cb7
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-09 15:07:08 -0400

    Solución: ReferenceError: fetch is not defined | con Vite

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['./jest-setup.cjs'], // (opcional)
+  setupFilesAfterEnv: ['./jest.setup.js'], // (opcional)
   // watchPathIgnorePatterns: ['<rootDir>/dist/','<rootDir>/node_modules/']
   transform: {
     "^.+\\.[t|j]sx?$": "babel-jest",
