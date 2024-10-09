c395034be6ef8b1cba72f41f6d9dac405bda1188
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-09 08:37:18 -0400

    Solución: No se puede usar `import.meta` fuera de un módulo (jest y vite)

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,8 @@
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['./jest-setup.cjs'], // (opcional)
-  watchPathIgnorePatterns: ['<rootDir>/dist/','<rootDir>/node_modules/']
+  // watchPathIgnorePatterns: ['<rootDir>/dist/','<rootDir>/node_modules/']
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest",
+  },
 };
