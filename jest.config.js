3604f87d5053e4aee8815cbc6a19906cddf15750
Juan Bautista Padilla <72109896+juanbpadilla@users.noreply.github.com>
2024-10-09 17:34:22 -0400

    Refactorizar nombres de archivos de configuración de Jest | Importar 'dotenv/config' a jest.setup.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,13 @@
-module.exports = {
+// module.exports = {
+export default {
+  // moduleNameMapper: {
+  //   "^@/(.*)$": "<rootDir>/src/$1",
+  // },
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['./jest.setup.js'], // (opcional)
   // watchPathIgnorePatterns: ['<rootDir>/dist/','<rootDir>/node_modules/']
   transform: {
     "^.+\\.[t|j]sx?$": "babel-jest",
+    "^.+\\.cjs$": "babel-jest"
   },
 };
